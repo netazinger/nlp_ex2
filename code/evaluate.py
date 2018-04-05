0b43785ee1b7a2c7d71e586e9e332d887bf28869
neta zinger <neta@singular.net>
2018-04-05 22:29:17 +0300

    Add evaluation funuction

--- code/evaluate.py
+++ code/evaluate.py
@@ -2,6 +2,34 @@
 import sys
 
 from consts import WordAndTag, Model
+import math
+
+def calc_word_accuracy_for_sentence(sentence, sentence_gold):
+    if len(sentence) != len(sentence_gold):
+        raise RuntimeError("sentences are not in same length")
+    num_of_true_tagging = 0
+    for i in range(sentence):
+        num_of_true_tagging += 1 if sentence[i] == sentence_gold[i] else 0
+    return num_of_true_tagging / len(sentence)
+
+
+def calc_sentence_accuracy_for_sentence(sentence, sentence_gold):
+    return int(math.floor(calc_word_accuracy_for_sentence(sentence, sentence_gold)))
+
+
+def cal_word_accuracy_for_corpus(test, gold):
+    if len(test) != len(gold):
+        raise RuntimeError("not in same length")
+
+    res = 0
+    for i in range(test):
+        res += calc_word_accuracy_for_sentence(test[i], gold[i]) * len(test[i])
+    return res / sum(map(len, test))
+
+def calc_sentence_accuracy_for_corpus(test, gold):
+    if len(test) != len(gold):
+        raise RuntimeError("not in same length")
+    return sum([calc_sentence_accuracy_for_sentence(test[i], gold[i]) for i in range(test)]) / len(test)
 
 
 def evaluate(tagged_file_path, model, gold_file_path, smoothing=False):
