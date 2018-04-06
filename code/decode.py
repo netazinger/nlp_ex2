56ed8d7ca495480d9a8e93c8cce4b3a263f9dbbb
neta zinger <neta@singular.net>
2018-04-06 11:15:35 +0300

    fix decode format

--- code/decode.py
+++ code/decode.py
@@ -7,12 +7,14 @@ from ex2.ex2 import read_train_baseline, tag_file
 from parse_data import read_test_file
 
 def print_word_and_tag(word_and_tag):
-    return "{word} {tag}".format(word=word_and_tag.word, tag=word_and_tag.tag)
+    return "{word}\t{tag}".format(word=word_and_tag.word, tag=word_and_tag.tag)
 
 def write_tagged_file(tagged_file, file_name):
     file = open(file_name + ".tagged","w")
     for tagged_sentance in tagged_file:
-        file.write("SEG " + " ".join(map(print_word_and_tag, tagged_sentance)) + '\n')
+        for word_and_tag in tagged_sentance:
+            file.write(print_word_and_tag(word_and_tag) + '\n')
+        file.write('\n')
     file.close()
 
 
@@ -22,13 +24,23 @@ def decode(model, test_file_path, param_files_path):
         raise RuntimeError("model %s is not supported. only support : %s" % (model, Model.ALL_MODELS))
     parsed_test_file = read_test_file(test_file_path)
 
-    _, file_name = os.path.split(test_file_path)
 
-    for param_file_path in param_files_path:
-        if model == Model.BASELINE:
-            segment_to_tag = read_train_baseline(param_files_path[0])
-            tagged_file = tag_file(parsed_test_file, segment_to_tag)
-            write_tagged_file(tagged_file, file_name)
+    _, file_name = os.path.split(test_file_path)
+    file_name, file_extension = os.path.splitext(file_name)
+
+
+    if model == Model.BASELINE:
+        segment_to_tag = read_train_baseline(param_files_path[0])
+        tagged_file = tag_file(parsed_test_file, segment_to_tag)
+
+        write_tagged_file(tagged_file, file_name)
+    elif model == Model.BI_GRAM:
+        pass
+    elif model == Model.TRI_GRAM:
+        pass
+
+
+
 
 
 def main():
