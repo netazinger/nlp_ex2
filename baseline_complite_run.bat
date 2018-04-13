c3c3df87270ed8258fd48fc4ebf66fbc91ed6532
neta zinger <neta@singular.net>
2018-04-13 15:40:45 +0300

    Baseline ready to run

--- baseline_complite_run.bat
+++ baseline_complite_run.bat
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 python train.py baseline data_training_files/heb-pos.train y
 python decode.py baseline data_files/heb-pos.test heb-pos.baseline
 python evaluate.py heb-pos.tagged data_files/heb-pos.gold baseline y
