78f51781456d427b8171521cb98c530fbd8d4e8e
neta zinger <neta@singular.net>
2018-04-13 16:11:28 +0300

    add bat file

---
+++ bi_gram_complite_run_no_smooth.bat
@@ -0,0 +1,4 @@
+python train.py bi-gram data_training_files/heb-pos.train n
+python decode.py bi-gram data_files/heb-pos.test heb-pos.gram heb-pos.lex
+python evaluate.py heb-pos.tagged data_files/heb-pos.gold bi-gram n
+
